1513da7178b9986cb17edcea8880a6322488b6ec
professionaltechs <professionalstechs@gmail.com>
2024-05-23 05:47:37 +0500

    chnages

--- src/app/components/StaticOverlay.js
+++ src/app/components/StaticOverlay.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import * as styles from "../styles/Portfolio.css";
-import art1 from "../Images/art1.jpg";
+import OS1 from "../Images/overlaystatic1.png";
+import OS2 from "../Images/overlaystatic2.png";
+import OS3 from "../Images/overlaystatic3.jpg";
+import OS4 from "../Images/overlaystatic4.jpg";
+import OS5 from "../Images/overlaystatic5.jpg";
+import OS6 from "../Images/overlaystatic6.jpg";
 import Image from "next/image";
 
 const StaticOverlay = () => {
@@ -11,22 +16,22 @@ const StaticOverlay = () => {
     </div>
     <div className="PortfolioContainer">
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={OS1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={OS2} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={OS3} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={OS4} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={OS5} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={OS6} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
     </div>
   </div></>
