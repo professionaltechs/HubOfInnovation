c135169141b7cffd75f8bb3af330272ada0b93ad
professionaltechs <professionalstechs@gmail.com>
2024-05-25 06:13:19 +0500

    chgksk

--- src/app/components/Emotes.js
+++ src/app/components/Emotes.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import * as styles from "../styles/Portfolio.css";
 import art1 from "../Images/art1.jpg";
+import emote1 from "../Images/emote1.jpg";
+import emote2 from "../Images/emote2.jpg";
+import emote3 from "../Images/emote3.jpg";
+import emote4 from "../Images/emote4.jpg";
+import emote5 from "../Images/emote5.png";
+import emote6 from "../Images/emote6.png";
+import ao from "../Images/emote6.png";
 import Image from "next/image";
 
 const Emotes = () => {
@@ -11,22 +18,22 @@ const Emotes = () => {
     </div>
     <div className="PortfolioContainer">
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={emote1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={emote2} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={emote3} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={emote4} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={emote5} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
       <div className="PortfolioCard">
-        <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
+        <Image src={emote6} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/>
       </div>
     </div>
   </div></>
