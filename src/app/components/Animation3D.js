07c950c112338beadd342486526b8c6226176714
professionaltechs <professionalstechs@gmail.com>
2024-05-31 20:37:11 +0500

    chnages

--- src/app/components/Animation3D.js
+++ src/app/components/Animation3D.js
@@ -1,40 +1,71 @@
-import React from 'react'
+import React from "react";
 import * as styles from "../styles/Portfolio.css";
 import art1 from "../Images/art1.jpg";
 // import art1 from "../Images/ao.mp4";
 import Image from "next/image";
-
 const Animation3D = () => {
   return (
-    <>    <div className="PortFolioSection">
-      <div className="PortfolioSectionTopHead">
-        <h1>3D Animation</h1>
+    <>
+      <div className="PortFolioSection">
+        <div className="PortfolioSectionTopHead">
+          <h1>3D Animation</h1>
+        </div>
+        <div className="PortfolioContainer">
+          <div className="PortfolioCard">
+            {/* <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/> */}
+            <video width="450" height="500px" controls  >
+              <source src="/ao.mp4" type="video/mp4" />
+            </video>
+          </div>
+          <div className="PortfolioCard">
+            <Image
+              src={art1}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="PortfolioCardImage"
+            />
+          </div>
+          <div className="PortfolioCard">
+            <Image
+              src={art1}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="PortfolioCardImage"
+            />
+          </div>
+          <div className="PortfolioCard">
+            <Image
+              src={art1}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="PortfolioCardImage"
+            />
+          </div>
+          <div className="PortfolioCard">
+            <Image
+              src={art1}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="PortfolioCardImage"
+            />
+          </div>
+          <div className="PortfolioCard">
+            <Image
+              src={art1}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="PortfolioCardImage"
+            />
+          </div>
+        </div>
       </div>
-      <div className="PortfolioContainer">
-        <div className="PortfolioCard">
-          {/* <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage"/> */}
-          <video width="450" height="500px" controls preload="none">
-            <source src="ao.mp4" type="video/mp4" />
-          </video>
-        </div>
-        <div className="PortfolioCard">
-          <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage" />
-        </div>
-        <div className="PortfolioCard">
-          <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage" />
-        </div>
-        <div className="PortfolioCard">
-          <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage" />
-        </div>
-        <div className="PortfolioCard">
-          <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage" />
-        </div>
-        <div className="PortfolioCard">
-          <Image src={art1} width={0} height={0} sizes="100vw" className="PortfolioCardImage" />
-        </div>
-      </div>
-    </div></>
-  )
-}
+    </>
+  );
+};
 
-export default Animation3D
+export default Animation3D;
